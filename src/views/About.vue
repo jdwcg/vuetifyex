2bd2f1caf51bdb28e5ea07ae7d3f7a848c485d3a
jdwnate <jdwcg@nate.com>
2023-11-23 14:11:53 +0900

    1

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<!-- About.vue -->
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // 컴포넌트 옵션
+};
+</script>
+
+<style>
+/* 스타일링 옵션 */
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<!-- Home.vue -->
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // 컴포넌트 옵션
+};
+</script>
+
+<style>
+/* 스타일링 옵션 */
+</style>
